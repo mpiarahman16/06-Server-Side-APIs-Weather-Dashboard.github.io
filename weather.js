b2e8f6de154f23814b56ec6f9f797634d267ebe1
mpiarahman16 <mpiarahman@gmail.com>
2020-04-09 14:02:11 -0400

    adding the weather dashboard

--- weather.js
+++ weather.js
@@ -1,59 +1,86 @@
 
-function buildQueryURL() {
-
-    var queryURL = "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22";
-    return queryURL
-}
 //settings.url = buildQueryURL()
 //$.ajax(settings).done(updateWeather);
+
+$("#search-button").on("click", function () {
+  var searchCity = $("#search-value").val();
+  $("#search-value").val("");
+  searchWeather(searchCity);
+  fiveDays(searchCity)
+})
 
 var APIKey = "9e615fcd5c8c91df3f82d284f5941fb2";
 
 // Here we are building the URL we need to query the database
 //var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid={your api key}=" + APIKey;
-searchWeather("Manhattan");
+
 
 // Here we run our AJAX call to the OpenWeatherMap API
 function searchWeather(searchCity) {
   console.log(searchCity);
   $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=9e615fcd5c8c91df3f82d284f5941fb2",
-        method: "GET", 
-        dataType: "json",
-      })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function(response) {
-      
-          // Log the queryURL
-          console.log(queryURL);
-      
-          // Log the resulting object
-          console.log(response);
-      
-          // Transfer content to HTML
-          $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-          $(".wind").text("Wind Speed: " + response.wind.speed);
-          $(".humidity").text("Humidity: " + response.main.humidity);
-          
-          // Convert the temp to fahrenheit
-          var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-      
-          // add temp content to html
-          $(".temp").text("Temperature (K) " + response.main.temp);
-          $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-      
-          // Log the data in the console as well
-          console.log("Wind Speed: " + response.wind.speed);
-          console.log("Humidity: " + response.main.humidity);
-          console.log("Temperature (F): " + tempF);
-        });
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=9e615fcd5c8c91df3f82d284f5941fb2",
+    method: "GET",
+    dataType: "json",
+  })
+
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+
+      // Convert the temp to fahrenheit
+      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+      // add temp content to html
+      $(".temp").text("Temperature (K) " + response.main.temp);
+      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + tempF);
+    });
+
+}
+function fiveDays(searchfiveDays) {
+  console.log(searchfiveDays);
+  $.ajax({
+    url: `http://api.openweathermap.org/data/2.5/forecast?q=${searchfiveDays}&appid=9e615fcd5c8c91df3f82d284f5941fb2`,
+    method: "GET",
+    dataType: "json",
+  })
+    .then(function (response) {
+      console.log(response);
+      var FiveDay = []
+      var fivedayList = response.list
+      for (var i = 0; i < fivedayList.length; i++) {
+        var timeoftheDay = fivedayList[i]
+        if (timeoftheDay.dt_txt.indexOf("15:00:00") >= 0) { FiveDay.push(timeoftheDay) }
+      }
+      for (var i = 0; i < FiveDay.length; i++) {
+        var divoneDay = $("<div>")
+        var heading = $("<h1>")
+        var temP = $("<p>")
+        var humiD = $("<p>")
+        heading.text(FiveDay[i].dt_txt)
+        // Convert the temp to fahrenheit
+        var tempF = (FiveDay[i].main.temp - 273.15) * 1.80 + 32;
+        temP.text("Temperature (F): " + tempF);
+        humiD.text("Humidity: " + FiveDay[i].main.humidity);
+        divoneDay.append(heading)
+        divoneDay.append(temP)
+        divoneDay.append(humiD)
+        $("#fivedayData").append(divoneDay)
         
-} 
-function updateWeather(WeatherData) {
-    var numTravelCards = "5"
-    for (var i = 0; i < NUMCARDS; i++) {
-        console.log("City;" + WeatherData[i].city.name)
-        console.log("Temperature;" + WeatherData[i].temperature)
-        console.log("Humidity;" + WeatherData[i].Humidity)
-        console.log("Wind Speed;" + WeatherData[i].windspeed)
-    }};
+      }
+    })
+}
+
+
